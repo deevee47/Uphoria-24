9586b942d70ee35f86bc9ddc717384d728bd587d
deevee <divyanshvishwakarma4@gmail.com>
2024-02-21 00:47:08 +0530

    mandala location fix

--- components/sub/LandingPageWheel.tsx
+++ components/sub/LandingPageWheel.tsx
@@ -6,7 +6,7 @@ import { slideInFromLeft } from '@/utils/motion'
 
 const LandingPageWheel = () => {
   return (
-    <motion.div initial= "hidden" animate= "visible"className="absolute top-[1%] left-[0%] rotate-270 w-[25%]">
+    <motion.div initial= "hidden" animate= "visible"className="absolute top-[0%] left-[0%] rotate-270 w-[25%]">
         <motion.div variants={slideInFromLeft(0.5)}>
         <Image className="fullmandala mt-[6vh]" src="ColoredWheel.svg" alt="Mandala SVG" width={450} height={150} />
         </motion.div>
