8e89230f327a2643748d7ca32960e898ee19a0e4
deevee <divyanshvishwakarma4@gmail.com>
2024-02-22 00:22:31 +0530

    tech team representation

---
+++ components/sub/tooltip.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React from "react";
+import { AnimatedTooltip } from "../ui/animated-tooltip";
+import Image from "next/image";
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    designation: "Software Engineer",
+    image:
+      "/dv.jpg",
+  },
+  {
+    id: 2,
+    name: "Robert Johnson",
+    designation: "Product Manager",
+    image:
+    "/dv.jpg"
+  },
+  {
+    id: 3,
+    name: "Jane Smith",
+    designation: "Data Scientist",
+    image:
+    "/dv.jpg"
+  },
+  {
+    id: 4,
+    name: "Emily Davis",
+    designation: "UX Designer",
+    image:
+    "/dv.jpg"
+  },
+  {
+    id: 5,
+    name: "Tyler Durden",
+    designation: "Soap Developer",
+    image:
+    "/dv.jpg"
+  },
+  {
+    id: 6,
+    name: "Divyansh Vishwakarma",
+    designation: "Full-Stack Developer",
+    image:
+    "/dv.jpg"
+  },
+];
+
+export function AnimatedTooltipPreview() {
+  return (
+    <div className="flex flex-row items-center justify-center mb-10 w-full">
+      <AnimatedTooltip items={people} />
+    </div>
+  );
+}
+
+export default AnimatedTooltipPreview;
