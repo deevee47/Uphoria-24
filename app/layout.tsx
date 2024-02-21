c321e4b86c06195783f3de39ca6dc83e6576fa86
deevee <divyanshvishwakarma4@gmail.com>
2024-02-21 23:48:33 +0530

    footbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="en">
+      <head><link href="https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css"rel="stylesheet"/></head>
       <body style={{ backgroundImage: `url('/background.svg')` }} className={`${inter.className} overflow-y-scroll overflow-x-hidden bg-cover bg-no-repeat object-cover bg-[#131313]`}>
          
          <Navbar />
