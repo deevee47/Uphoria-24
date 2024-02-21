1f13a1cd783d115657a78d2c3e3f917a0492ee01
deevee <divyanshvishwakarma4@gmail.com>
2024-02-21 16:19:16 +0530

    added side golden mandala, responsive

--- components/sub/TimelineContent.tsx
+++ components/sub/TimelineContent.tsx
@@ -85,23 +85,29 @@ const TimelineContent: React.FC = () => {
         {events15.map((event, index) => (
             <EventItem key={index} time={event.time} eventName={event.eventName} />
         ))}
+            <div className="goldenmandala absolute -top-[45%] rotate-180 left-[0%] w-[30%] h-full flex justify-end">
+              <Image src="goldenMandala.svg" alt="Golden Mandala" width={450} height={60} />
+            </div>
           <div className='flex flex-col absolute mt-[30vw] top-[6%] left-[10%] sm:top-[0%] sm:left-[10%]'>
             <div className='eventdate text-[16vw] sm:text-[15vw] font-normal'>15</div>
             <div className='eventmonth text-[3.5vw] sm:text-[3vw] font-bold text-[#FFAD0E]'>March <br></br>2024</div>
           </div>
-          <Image src="Line.svg" alt="Line SVG" width={850} height={60} className='py-[2vw]'/>
+          <Image src="Line.svg" alt="Line SVG" width={270} height={60} className='py-[4vw] sm:py-[2vw] sm:w-[50%]'/>
 
-          {/* <Image src="SideWheel.svg" alt="SideWheel SVG" width={550} height={60}/> */}
+          {/* <Image className=" items-end absolute top-[0%]" src="SideWheel.svg" alt="SideWheel SVG" width={550} height={60}/> */}
           
 
           {events16.map((event, index) => (
             <EventItem key={index} time={event.time} eventName={event.eventName} />
       ))}
+            <div className="goldenmandala absolute -top-[5%] left-[70%] w-[30%] h-full flex justify-end">
+              <Image src="goldenMandala.svg" alt="Golden Mandala" width={450} height={60} />
+            </div>
           <div className='flex flex-col  absolute mt-[90vw] top-[16%] left-[10%] sm:top-[10%] sm:left-[10%]'>
             <div className='eventdate text-[16vw] sm:text-[15vw] font-normal'>16</div>
             <div className='eventmonth text-[3.5vw] sm:text-[3vw] font-bold text-[#FFAD0E]'>March <br></br>2024</div>
           </div>
-          <Image src="Line.svg" alt="Line SVG" width={850} height={60} className='py-[2vw]'/>
+          <Image src="Line.svg" alt="Line SVG" width={270} height={60} className='py-[4vw] sm:py-[2vw] sm:w-[50%]'/>
 
         {events17.map((event, index) => (
         <EventItem key={index} time={event.time} eventName={event.eventName} />
