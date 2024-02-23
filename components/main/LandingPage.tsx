7ba797cc182fbda53ddbb826a0b7788d931f3021
mach-12 <mann.compi@gmail.com>
2024-02-24 00:46:40 +0530

    formatting

--- components/main/LandingPage.tsx
+++ components/main/LandingPage.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import LandingPageContent from '../sub/LandingPageContent';
-import LandingPageWheel from '../sub/LandingPageWheel';
-import Navbar from './Navbar';
+import React from "react";
+import LandingPageContent from "../sub/LandingPageContent";
+import LandingPageWheel from "../sub/LandingPageWheel";
+import Navbar from "./Navbar";
 
 const LandingPage = () => {
   return (
-    <div className='relative flex flex-col items-center justify-center h-full w-full'>    
+    <div className="relative flex flex-col items-center justify-center h-full w-full">
       <LandingPageContent />
       <LandingPageWheel />
     </div>
-  )
-}
+  );
+};
 
 export default LandingPage;
