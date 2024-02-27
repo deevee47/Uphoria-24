b50b1bf37d56e2c17049724c269647400011093d
Divyansh Vishwakarma <119093182+deevee47@users.noreply.github.com>
2024-02-27 11:49:17 +0530

    Update animated-tooltip.tsx

--- components/ui/animated-tooltip.tsx
+++ components/ui/animated-tooltip.tsx
@@ -71,7 +71,7 @@ export const AnimatedTooltip = ({
               >
                 <div className="absolute inset-x-10 z-30 w-[20%] -bottom-px bg-gradient-to-r from-transparent via-[#F58220] to-transparent h-px " />
                 <div className="absolute left-10 w-[40%] z-30 -bottom-px bg-gradient-to-r from-transparent via-[#F58220] to-transparent h-px " />
-                <div className="font-bold relative z-30 sm:text-base text-[3vw]">
+                <div className="font-bold relative z-30 sm:text-base text-[2.8vw]">
                   <a href={item.url} target="_blank">{item.name}</a>
                 </div>
                 <div className="text-white text-xs">{item.designation}</div>
