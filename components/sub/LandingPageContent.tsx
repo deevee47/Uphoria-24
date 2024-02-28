8e3ed11c0ccd07de5aa6e54a5a7a8f64898db2d9
deevee47 <divyanshvishwakarma4@gmail.com>
2024-02-28 21:59:45 +0530

    mandala resized, uphoria logo padding adjustment

--- components/sub/LandingPageContent.tsx
+++ components/sub/LandingPageContent.tsx
@@ -11,7 +11,7 @@ const LandingPageContent = () => {
 
             <motion.div 
              variants={slideInFromTop}
-             className='UphoriaBox pt-[24vw] w-fit sm:w-[100%]'>
+             className='UphoriaBox pt-[36vw] sm:pt-[24vw]  w-fit sm:w-[100%]'>
               <Image className="UphoriaLogo mb-[2vw] sm:mt-[2vw] sm:ml-[29vw] animate-pulse" src="UphoriaGlow.svg" alt="Example SVG" width={750} height={200} loading="eager"/>
               <a href='#events' className='hidden  sm:flex w-fit text-center justify-center items-center explore ml-[65vw] px-[0.7vw] py-[0.1vw] pt-[0.3vw] text-[1.2vw] bg-[#FFAD0ECC] rounded-xl hover:bg-transparent hover:text-[#FFAD0E]'>Explore</a>
             </motion.div>
