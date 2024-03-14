2e14d91d43456666e1db6ca7f13ac990dbdc7c11
deevee47 <divyanshvishwakarma4@gmail.com>
2024-03-14 16:57:33 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/main/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,7 @@ export default function RootLayout({
          
          <Navbar />
          {children}
-
+         <Analytics />
       </body>
     </html>
   );
